bfe156c0800f036934d78b20e96d199a689503d2
fsdmern <fsd.mern@gmail.com>
2022-10-21 08:29:01 +0530

    callback promises async/await fetch covered

--- index.js
+++ index.js
@@ -59,10 +59,11 @@ document.write('<p><strong>Try Catch </strong></p>');
 //   alert('An error occured inside try block: ' + e);
 // }
 
-//Promises, Fetch, Async/Await
-document.write('<p><strong>Promises, Fetch, Async/Await </strong></p>');
+//Callback and Promises
+document.write('<h3><strong>Callback</strong></h3>');
+
 //Call back function
-document.write('<p><strong>Callback Function </strong></p>');
+
 //A function will be passed to another function as an argument
 // function greet(name) {
 //   alert(`Hello, ${name}`); //back ticks are called as template literals
@@ -74,6 +75,42 @@ document.write('<p><strong>Callback Function </strong></p>');
 // }
 
 // processUserInput(greet);
+//Calling Functions Independently
+function myPrint(msg) {
+  document.getElementById('test1').innerHTML = msg;
+}
+
+function myCalc(n1, n2) {
+  return n1 + n2;
+}
+
+let result = myCalc(10, 15);
+myPrint(result);
+
+//Calling Function within another function
+function myPrint1(msg) {
+  document.getElementById('test2').innerHTML = msg;
+}
+
+function myCalc1(n1, n2) {
+  let sum = n1 + n2;
+  myPrint1(sum);
+}
+
+myCalc1(15, 25);
+
+//Calling functions using callback
+function myPrint2(msg) {
+  document.getElementById('test3').innerHTML = msg;
+}
+
+function myCalc2(n1, n2, someFunc) {
+  let sum = n1 + n2;
+  someFunc(sum);
+}
+myCalc2(10, 20, myPrint2);
+
+document.write('<h3><strong>Promises</strong></h3>');
 //states: pending, fullfilled or rejected
 //result: undefined, value (when resolved), error (when rejected)
 let promise = new Promise(function (resolve, reject) {
@@ -125,6 +162,62 @@ myPromise3
     console.log(result2);
   });
 
+//Async/Await
+//Giving some work to a program. Program says I will finish whenever possible and report back
+//Async functions are a higher level abstraction over promises.
+document.write('<p><strong>Async/Await</strong></p>');
+async function test1() {
+  return 'This is an async function';
+}
+
+test1().then((result) => {
+  document.write('Inside an async function: ' + result);
+});
+
+// async function test2() {
+//   let promise = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve('My work is done!'), 2000);
+//   });
+//   let result = await promise;
+//   alert(result);
+// }
+
+// test2();
+
+//Fetch
+document.write('<p><strong>Fetch</strong></p>');
+let url = 'https://restcountries.com/v3.1/all';
+async function fecthFunction(url) {
+  const response = await fetch(url);
+  let data = await response.json();
+  console.log(data);
+}
+
+fecthFunction(url);
+
+//Difference between object format and json format
+// obj = {firsName: "John", age: 25}, json = {"firstName":"John","age","25"}
+
+//Scope and Hoisting
+document.write('<p><strong>Scope and Hoisting</strong></p>');
+//Scope: Local and Global
+//Hoisting: All global variable declarations made across the program will go to the beginning of the execution and memory allocated
+
+//Classes
+
+//Object destructuring
+
+//Rest Operator
+
+//Spread Operator
+
+//Object Accessors, Constructors, Prototypes
+
+//Regular Expressions
+//search() replace() and modifiers
+
+//JSON
+
 //DOM (selecting various html elements)
 
 //Window, Click and other Event Listeners
@@ -132,5 +225,3 @@ myPromise3
 //Modules (Export Import)
 
 //Cookies, sessionStorage
-
-//Java Script Testing (automated testing with Mocha)
